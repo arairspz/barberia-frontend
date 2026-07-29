a56b65ccf272058f93a1e67fcf68d35fa2ea9de0
arairspz <rodrigoeulogio1445@gmail.com>
2026-07-29 16:35:49 -0500

    problema de not found era porque faltaba a que direccion

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 const ProductList = () => {
     const [productos, setProductos] = useState([]);
     const [error, setError] = useState(null);
-    const URL = "https://voicing-bobtail-bankbook.ngrok-free.dev"; 
+    const URL = "https://voicing-bobtail-bankbook.ngrok-free.dev/productos"; 
 
     useEffect(() => {
         const fetchProductos = async () => {
